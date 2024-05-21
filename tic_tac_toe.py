9ebeae4551940776480888dc014f49dbfeae5c4f
Parker <pkroberg@gmail.com>
2024-05-21 00:38:32 -0600

    Add check for win

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -20,6 +20,18 @@ turn = 1
 
 #game loop
 while not game_over:
+    #check for win
+    if board[1] + board[5] + board[9] == "xxx" or board[1] + board[14] + board[27] == "xxx" or board[1] + board[18] + board[35] == "xxx" or board[5] + board[14] + board[31] == "xxx" or board[9] + board[18] + board[27] == "xxx" or board[9] + board[22] + board[35] == "xxx" or board[14] + board[18] + board[22] == "xxx" or board[27] + board[31] + board[35] == "xxx":
+        print(board)
+        print("Player x wins!")
+        game_over = True
+        break
+    if board[1] + board[5] + board[9] == "ooo" or board[1] + board[14] + board[27] == "ooo" or board[1] + board[18] + board[35] == "ooo" or board[5] + board[14] + board[31] == "ooo" or board[9] + board[18] + board[27] == "ooo" or board[9] + board[22] + board[35] == "ooo" or board[14] + board[18] + board[22] == "ooo" or board[27] + board[31] + board[35] == "ooo":
+        print(board)
+        print("Player o wins!")
+        game_over = True
+        break
+
     #player 1 character selection
     if player_1_character == "":
         player_1_character = input("Player 1, would you like to be 'x' or 'o'?").lower()
